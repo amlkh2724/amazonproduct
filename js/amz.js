de5d51458858438afdf5ed453d5bccacf8fc54f3
amal khanjer <amlkh2724@gmail.com>
2023-01-13 17:02:35 +0200

    imgshover

---
+++ js/amz.js
@@ -0,0 +1,9 @@
+const big_img = document.getElementById("img-big");
+const small_img = document.getElementById("img-small-1");
+
+document.getElementById("img-small-1").addEventListener("mouseover", function () {
+    document.getElementById("big-hover").style.background = "url('/small-1.jpg')";
+
+}, false);
+
+
